a1071180bcae59f056809928e1291f0c9b5e47a3
digoumenidis <digoumenidis@egritosgroup.gr>
2018-06-18 12:24:01 +0300

    Add project files.

---
+++ hello/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this is sleep calculator!");
+
+            Console.WriteLine("How many hours did you sleep last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            if (hoursOfSleep >= 8)
+            {
+                Console.WriteLine("WELL DONE");
+
+            }
+            else
+            {
+                Console.WriteLine("NOOOO");
+            }
+        }
+    }
+}
